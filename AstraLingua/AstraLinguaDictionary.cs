7af7964280583c7ab730681804d7bcc36a999721
Joyless <65855333+Joy-less@users.noreply.github.com>
2026-03-21 16:14:03 +0000

    Add extra dictionary helpers

--- AstraLingua/AstraLinguaDictionary.cs
+++ AstraLingua/AstraLinguaDictionary.cs
@@ -1,6 +1,9 @@
 namespace AstraLingua;
 
 public static partial class AstraLinguaDictionary {
+    public static string GetWord(int NumberCode) {
+        return Dictionary[NumberCode];
+    }
     public static string GetWordName(scoped ReadOnlySpan<char> Word) {
         int OpenBracketIndex = Word.IndexOf('(');
         if (OpenBracketIndex >= 0) {
@@ -10,6 +13,9 @@ public static partial class AstraLinguaDictionary {
         Word = Word.Trim();
 
         return Word.ToString();
+    }
+    public static string GetWordName(int NumberCode) {
+        return GetWordName(Dictionary[NumberCode]);
     }
     public static string GetWordDescription(scoped ReadOnlySpan<char> Word) {
         int OpenBracketIndex = Word.IndexOf('(');
@@ -23,5 +29,8 @@ public static partial class AstraLinguaDictionary {
         Word = Word.Trim();
 
         return Word.ToString();
+    }
+    public static string GetWordDescription(int NumberCode) {
+        return GetWordDescription(Dictionary[NumberCode]);
     }
 }
